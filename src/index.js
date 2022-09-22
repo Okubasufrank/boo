3cfac6f1101a8eb3ab6c63c5b88d0b7baf4683e2
Okubasufrank <franklineokubasu51@gmail.com>
2022-09-22 13:19:34 +0300

    created react App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css';
 
 
-
+ReactDOM.render(<App/>, document.getElementById('root'))
 
